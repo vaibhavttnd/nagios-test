d7b7f93527724ddd7c15aaa823e4d3565480b89f
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-15 21:50:27 +0530

    adasd

--- cookbook-fen/recipes/csm_sync.rb
+++ cookbook-fen/recipes/csm_sync.rb
@@ -12,15 +12,15 @@ end
 
 
 
-env = { AWS_DEFAULT_REGION: 'us-east-1' }
-exepath = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+#env = { AWS_DEFAULT_REGION: 'us-east-1' }
+#exepath = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 
-cron 'learning-cms-assets-code-deployment' do
-  environment env
-  path exepath
-  day '*/15'
-  command 'bash /home/monitoring/learning-cms-assets-code-deployment.sh'
-  user node['web_app']['user_name']
+#cron 'learning-cms-assets-code-deployment' do
+#  environment env
+#  path exepath
+#  day '*/15'
+#  command 'bash /home/monitoring/learning-cms-assets-code-deployment.sh'
+#  user node['web_app']['user_name']
 #  mailto 'system-alerts@fen.com'
-end
+#end
