d1b69c3a41f6fd0c603aa46034f488ae7ebf9ad6
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-16 11:32:36 +0530

    adasd

--- cookbook-fen/attributes/default.rb
+++ cookbook-fen/attributes/default.rb
@@ -1,3 +1,3 @@
-default['web_app']['user_name'] = "monitoring2"
-default['web_app']['group_name'] = "monitoring2"
+default['web_app']['user_name'] = "monitoring3"
+default['web_app']['group_name'] = "monitoring3"
 default['web_app']['user_dir'] = "/opt"
--- cookbook-fen/recipes/user.rb
+++ cookbook-fen/recipes/user.rb
@@ -1,19 +1,12 @@
-#use monitoring via variable
 user "#{node['web_app']['user_name']}" do
   comment 'Monitoring User'
-  uid '1240'
-  home '/opt' # change home directory
+  uid '1233'
+  home "#{node['web_app']['user_dir']}"
   shell '/bin/bash'
   supports :manage_home => true
   action :create
 end
 #################### User created
-
-#in attributes
-
-#node.default['web_app']['user_name'] = "monitoring"
-#node.default['web_app']['group_name'] = "monitoring"
-#node.default['web_app']['user_dir'] = "/opt"
 
 directory "#{node['web_app']['user_dir']}/.ssh" do
   mode 0775
