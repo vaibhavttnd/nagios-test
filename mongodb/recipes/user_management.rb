1a14291773438eae666ef3726673e62adb20f563
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-26 20:55:13 +0530

    yum

--- mongodb/recipes/user_management.rb
+++ mongodb/recipes/user_management.rb
@@ -5,7 +5,8 @@ admin = node['mongodb']['admin']
 
 # If authentication is required,
 # add the admin to the users array for adding/updating
-users << admin if node['mongodb']['config']['auth'] == true
+
+users << admin #if node['mongodb']['config']['auth'] == true
 
 users.concat(node['mongodb']['users'])
 
