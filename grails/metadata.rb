06241b934fceafffa69a9add3857c39dca6cad97
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-28 22:04:42 +0530

    grails-2.5.1

---
+++ grails/metadata.rb
@@ -0,0 +1,15 @@
+name             'spinen-grails'
+maintainer       'SPINEN'
+maintainer_email  'luke.reimer@spinen.com' 
+license          'MIT'
+description      'Installs/Configures grails'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+
+%w(
+  ark
+).each do |cookbook|
+  depends cookbook
+end
+
+supports 'ubuntu'
