46b4fa9d4ddf838a570fb4fcd9bac17d3fa15708
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-28 21:48:48 +0530

    checksum removed

--- spinen-grails/attributes/default.rb
+++ spinen-grails/attributes/default.rb
@@ -11,4 +11,4 @@
 default['grails']['version'] = '2.5.1'#vaibhav
 default['grails']['home'] = '/usr/local/grails'
 default['grails']['url'] = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-#{node['grails']['version']}.zip"
-default['grails']['checksum'] = 'c53e01a9d98c499f91d7c7d54312dbdfd33f99ffcdec8bc4946036e4bea2c8e1'
+#default['grails']['checksum'] = 'c53e01a9d98c499f91d7c7d54312dbdfd33f99ffcdec8bc4946036e4bea2c8e1'
--- spinen-grails/recipes/default.rb
+++ spinen-grails/recipes/default.rb
@@ -12,7 +12,7 @@ include_recipe 'ark::default'
 
 ark 'grails' do
   url node['grails']['url']
-  checksum node['grails']['checksum']
+#  checksum node['grails']['checksum']
   home_dir node['grails']['home']
   version node['grails']['version']
 end
